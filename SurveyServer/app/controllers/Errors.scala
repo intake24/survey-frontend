e1b219dae8f88043edbe5403c862dcfe3543fa29
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2017-03-16 11:58:49 +0000

    Forward error reports to API server

--- SurveyServer/app/controllers/Errors.scala
+++ SurveyServer/app/controllers/Errors.scala
@@ -2,48 +2,68 @@ package controllers
 
 import javax.inject.Inject
 
+import akka.util.CompactByteString
 import com.google.gwt.core.server.StackTraceDeobfuscator
-import play.api.{Configuration, Logger}
-import play.api.libs.ws.WSClient
-import play.api.mvc.{Action, BodyParsers, Controller}
+import play.api.Configuration
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.ws.{InMemoryBody, WSClient}
+import play.api.mvc.{Action, Controller}
 import upickle.default._
 
+import scala.collection.mutable
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
-case class ClientStackTraceElement(className: String, fileName: String, lineNumber: Int, methodName: String) {
-  def toStackTraceElement = new StackTraceElement(className, methodName, fileName, lineNumber)
-}
-
-case class ClientException(className: String, message: String, stackTrace: List[ClientStackTraceElement])
-
-case class ErrorReport(surveyId: String, userName: Option[String], gwtPermutationStrongName: String, exceptionChain: List[ClientException])
-
-case class ForwardErrorReport(surveyId: String, userName: Option[String], exceptionChain: List[ClientException])
 
 class Errors @Inject()(config: Configuration, ws: WSClient) extends Controller with UpickleUtil {
 
   private val apiBaseUrl = config.getString("intake24.apiBaseUrl").get
 
-  val deobfuscator = StackTraceDeobfuscator.fromResource("META-INF/gwt-deploy/survey/symbolMaps")
+  case class ClientStackTraceElement(className: String, fileName: String, lineNumber: Int, methodName: String) {
+    def toStackTraceElement = new StackTraceElement(className, methodName, fileName, lineNumber)
+  }
+
+  case class ClientException(className: String, message: String, stackTrace: List[ClientStackTraceElement])
+
+  case class ErrorReport(surveyId: String, userName: Option[String], gwtPermutationStrongName: String, exceptionChain: Seq[ClientException], surveyStateJSON: String)
+
+  private case class ForwardErrorReport(userId: Option[String], surveyId: Option[String], stackTrace: Seq[String], surveyStateJSON: String)
+
+  private val deobfuscator = StackTraceDeobfuscator.fromResource("META-INF/gwt-deploy/survey/symbolMaps")
+
+  private def buildStackTrace(gwtPermutation: String, exceptionChain: Seq[ClientException]): Future[Seq[String]] =
+    Future {
+      val stackTrace = mutable.Buffer[String]()
+
+      exceptionChain.foreach {
+        exception =>
+
+          val exceptionDesc = s"${exception.className}: ${exception.message}"
+
+          if (!stackTrace.isEmpty)
+            stackTrace.append(s"Caused by $exceptionDesc")
+          else
+            stackTrace.append(s"Exception $exceptionDesc")
+
+          exception.stackTrace.foreach {
+            ste =>
+              val d = deobfuscator.resymbolize(ste.toStackTraceElement, gwtPermutation)
+
+              stackTrace.append(s"  at ${d.getClassName}.${d.getMethodName}(${d.getFileName}:${d.getLineNumber})")
+          }
+      }
+
+      stackTrace
+    }
 
   def report() = Action.async(upickleBodyParser[ErrorReport]) {
     request =>
-      Future {
-
-        val report = request.body
-
-        val deobfuscatedChain = report.exceptionChain.map {
-          exception =>
-            val deobfuscatedStackTrace = exception.stackTrace.map {
-              ste =>
-                val d = deobfuscator.resymbolize(ste.toStackTraceElement, report.gwtPermutationStrongName)
-                ClientStackTraceElement(d.getClassName, d.getFileName, d.getLineNumber, d.getMethodName)
-            }
-            exception.copy(stackTrace = deobfuscatedStackTrace)
-        }
-
-        Ok(write(ForwardErrorReport(report.surveyId, report.userName, deobfuscatedChain)))
-      }
+      for (stackTrace <- buildStackTrace(request.body.gwtPermutationStrongName, request.body.exceptionChain);
+           apiResponse <- ws.url(s"$apiBaseUrl/errors/report")
+             .withRequestTimeout(10 seconds)
+             .withHeaders(("Content-Type", "application/json"))
+             .withBody(InMemoryBody(CompactByteString(write(ForwardErrorReport(request.body.userName, Some(request.body.surveyId), stackTrace, request.body.surveyStateJSON)))))
+             .execute("POST")
+      ) yield Status(apiResponse.status)
   }
 }
