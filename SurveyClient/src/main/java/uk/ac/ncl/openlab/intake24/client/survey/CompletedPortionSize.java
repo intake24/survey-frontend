bcb2407c532927a1641452022de288cc2b934da2
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2018-02-07 13:22:07 +0000

    Fix NPE when applying conversion factors

--- SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/survey/CompletedPortionSize.java
+++ SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/survey/CompletedPortionSize.java
@@ -26,88 +26,100 @@ http://www.nationalarchives.gov.uk/doc/open-government-licence/
 
 package uk.ac.ncl.openlab.intake24.client.survey;
 
+import uk.ac.ncl.openlab.intake24.client.BrowserConsole;
+import uk.ac.ncl.openlab.intake24.client.api.errors.ErrorReportingService;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class CompletedPortionSize {
-  public String method;
-  public Map<String, String> data;
+    public String method;
+    public Map<String, String> data;
 
-  @Deprecated
-  public CompletedPortionSize() {
+    @Deprecated
+    public CompletedPortionSize() {
 
-  }
+    }
 
-  public CompletedPortionSize(String method, Map<String, String> data) {
-    this.method = method;
-    this.data = data;
-  }
+    public CompletedPortionSize(String method, Map<String, String> data) {
+        this.method = method;
+        this.data = data;
+    }
 
-  public double servingWeight() {
-    return Double.parseDouble(data.get("servingWeight"));
-  }
+    public double servingWeight() {
+        String str = data.get("servingWeight");
 
-  public double leftoversWeight() {
-    return Double.parseDouble(data.get("leftoversWeight"));
-  }
+        if (str == null)
+            return 0;
+        else
+            return Double.parseDouble(str);
+    }
 
-  public CompletedPortionSize multiply(double x) {
-    HashMap<String, String> newData = new HashMap<String, String>(data);
-    
-    newData.put("servingWeight", Double.toString(servingWeight() * x));
-    newData.put("leftoversWeight", Double.toString(leftoversWeight() * x));
-    
-    return new CompletedPortionSize(method, newData);
-  }
+    public double leftoversWeight() {
+        String str = data.get("leftoversWeight");
+        if (str == null)
+            return 0;
+        else
+            return Double.parseDouble(str);
+    }
 
-  public static CompletedPortionSize ignore(String reason) {
-    Map<String, String> data = new HashMap<String, String>();
+    public CompletedPortionSize multiply(double x) {
+        HashMap<String, String> newData = new HashMap<String, String>(data);
 
-    data.put("reason", reason);
-    data.put("servingWeight", "0.0");
-    data.put("leftoversWeight", "0.0");
+        newData.put("servingWeight", Double.toString(servingWeight() * x));
+        newData.put("leftoversWeight", Double.toString(leftoversWeight() * x));
 
-    return new CompletedPortionSize("ignored", data);
-  }
+        return new CompletedPortionSize(method, newData);
+    }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((data == null) ? 0 : data.hashCode());
-    result = prime * result + ((method == null) ? 0 : method.hashCode());
-    return result;
-  }
+    public static CompletedPortionSize ignore(String reason) {
+        Map<String, String> data = new HashMap<String, String>();
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    CompletedPortionSize other = (CompletedPortionSize) obj;
+        data.put("reason", reason);
+        data.put("servingWeight", "0.0");
+        data.put("leftoversWeight", "0.0");
 
-    // System.out.println("CPS begin");
+        return new CompletedPortionSize("ignored", data);
+    }
 
-    if (data == null) {
-      if (other.data != null)
-        return false;
-    } else if (!data.equals(other.data))
-      return false;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        result = prime * result + ((method == null) ? 0 : method.hashCode());
+        return result;
+    }
 
-    // System.out.println("CPS data ok");
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CompletedPortionSize other = (CompletedPortionSize) obj;
 
-    if (method == null) {
-      if (other.method != null)
-        return false;
-    } else if (!method.equals(other.method))
-      return false;
+        // System.out.println("CPS begin");
 
-    // System.out.println("CPS script OK");
+        if (data == null) {
+            if (other.data != null)
+                return false;
+        } else if (!data.equals(other.data))
+            return false;
 
-    return true;
-  }
+        // System.out.println("CPS data ok");
+
+        if (method == null) {
+            if (other.method != null)
+                return false;
+        } else if (!method.equals(other.method))
+            return false;
+
+        // System.out.println("CPS script OK");
+
+        return true;
+    }
 
 }
