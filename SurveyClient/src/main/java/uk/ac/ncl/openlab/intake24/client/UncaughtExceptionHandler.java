b2047d98ce24c1c4ff0c8e7fe1e6f1116dd423cb
Ivan Poliakov <ivpolyakov@gmail.com>
2018-04-13 12:38:43 +0100

    Prevent endless error reporting loops

--- SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/UncaughtExceptionHandler.java
+++ SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/UncaughtExceptionHandler.java
@@ -1,17 +1,71 @@
 package uk.ac.ncl.openlab.intake24.client;
 
 import com.google.gwt.core.client.GWT;
+import org.fusesource.restygwt.client.Method;
+import org.fusesource.restygwt.client.MethodCallback;
+import org.workcraft.gwt.shared.client.Option;
+import uk.ac.ncl.openlab.intake24.client.api.auth.AuthCache;
+import uk.ac.ncl.openlab.intake24.client.api.errors.ErrorReport;
 import uk.ac.ncl.openlab.intake24.client.api.errors.ErrorReportingService;
+import uk.ac.ncl.openlab.intake24.client.api.errors.SThrowable;
+import uk.ac.ncl.openlab.intake24.client.survey.StateManagerUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UncaughtExceptionHandler implements GWT.UncaughtExceptionHandler {
 
-    public static final UncaughtExceptionHandler INSTANCE = new UncaughtExceptionHandler();
+  public static final UncaughtExceptionHandler INSTANCE = new UncaughtExceptionHandler();
 
-    @Override
-    public void onUncaughtException(final Throwable e) {
-        GWT.log("Uncaught exception", e);
+  private static boolean reportErrorInProgress = false;
 
-        ErrorReportingService.reportError(e);
+  public static void reportError(Throwable exception) {
 
+    if (reportErrorInProgress) {
+      GWT.log("Unhandled exception occurred during error reporting, will not report to avoid loops", exception);
+      reportErrorInProgress = false;
+      return;
     }
+
+    String surveyStateJSON = "{}";
+
+    String surveyId = EmbeddedData.surveyId;
+    Option<String> userId = AuthCache.getCurrentUserIdOption();
+
+    if (!userId.isEmpty())
+      surveyStateJSON = StateManagerUtil.getLatestStateSerialised(userId.getOrDie()).getOrElse("{}");
+
+    Throwable cur = exception;
+
+    List<SThrowable> exceptionChain = new ArrayList<>();
+
+    while (cur != null) {
+      exceptionChain.add(new SThrowable(cur));
+      cur = cur.getCause();
+    }
+
+    reportErrorInProgress = true;
+
+    ErrorReportingService.INSTANCE.reportError(new ErrorReport(surveyId, userId, GWT.getPermutationStrongName(), exceptionChain, surveyStateJSON), new MethodCallback<Void>() {
+      @Override
+      public void onFailure(Method method, Throwable errorServiceException) {
+        GWT.log("Failed to report exception", errorServiceException);
+        reportErrorInProgress = false;
+
+      }
+
+      @Override
+      public void onSuccess(Method method, Void response) {
+        GWT.log("Reported " + exception.getClass().getName());
+        reportErrorInProgress = false;
+      }
+    });
+  }
+
+  @Override
+  public void onUncaughtException(final Throwable e) {
+    GWT.log("Uncaught exception", e);
+
+    reportError(e);
+  }
 }
--- SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/api/auth/RefreshCallback.java
+++ SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/api/auth/RefreshCallback.java
@@ -4,104 +4,102 @@ import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
-import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
-import org.apache.jasper.tagplugins.jstl.core.Url;
 import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.MethodCallback;
 import org.workcraft.gwt.shared.client.Callback;
 import org.workcraft.gwt.shared.client.Callback1;
 import uk.ac.ncl.openlab.intake24.client.EmbeddedData;
-import uk.ac.ncl.openlab.intake24.client.api.errors.ErrorReportingService;
+import uk.ac.ncl.openlab.intake24.client.UncaughtExceptionHandler;
 
 import java.util.logging.Logger;
 
 public class RefreshCallback implements RequestCallback {
 
-    private static final Logger logger = Logger.getLogger(RefreshCallback.class.getName());
+  private static final Logger logger = Logger.getLogger(RefreshCallback.class.getName());
 
-    private final Method refreshMethod;
-    private final RequestCallback refreshCallback;
-    private final SigninUI loginUI;
-    private final GenUserUI genUserUI;
-    private final AuthTokenUI authTokenUI;
+  private final Method refreshMethod;
+  private final RequestCallback refreshCallback;
+  private final SigninUI loginUI;
+  private final GenUserUI genUserUI;
+  private final AuthTokenUI authTokenUI;
 
 
-    public RefreshCallback(Method method, RequestCallback userCallback, SigninUI loginUI, GenUserUI genUserUI, AuthTokenUI authTokenUI) {
-        this.refreshMethod = method;
-        this.refreshCallback = userCallback;
-        this.loginUI = loginUI;
-        this.genUserUI = genUserUI;
-        this.authTokenUI = authTokenUI;
-    }
+  public RefreshCallback(Method method, RequestCallback userCallback, SigninUI loginUI, GenUserUI genUserUI, AuthTokenUI authTokenUI) {
+    this.refreshMethod = method;
+    this.refreshCallback = userCallback;
+    this.loginUI = loginUI;
+    this.genUserUI = genUserUI;
+    this.authTokenUI = authTokenUI;
+  }
 
-    @Override
-    public void onResponseReceived(final Request request, Response response) {
-        int code = response.getStatusCode();
+  @Override
+  public void onResponseReceived(final Request request, Response response) {
+    int code = response.getStatusCode();
 
-        if (code == Response.SC_UNAUTHORIZED) {
+    if (code == Response.SC_UNAUTHORIZED) {
 
-            if (Window.Location.getParameter(UrlParameterConstants.generateUserKey) != null) {
-                genUserUI.show();
+      if (Window.Location.getParameter(UrlParameterConstants.generateUserKey) != null) {
+        genUserUI.show();
 
-                GeneratedUsersService.INSTANCE.generateUser(EmbeddedData.surveyId, new MethodCallback<GeneratedCredentials>() {
-                    @Override
-                    public void onFailure(Method method, Throwable exception) {
-                        if (method.getResponse().getStatusCode() == 403)
-                            genUserUI.onForbidden();
-                        else
-                            genUserUI.onServiceError();
-                    }
+        GeneratedUsersService.INSTANCE.generateUser(EmbeddedData.surveyId, new MethodCallback<GeneratedCredentials>() {
+          @Override
+          public void onFailure(Method method, Throwable exception) {
+            if (method.getResponse().getStatusCode() == 403)
+              genUserUI.onForbidden();
+            else
+              genUserUI.onServiceError();
+          }
 
-                    @Override
-                    public void onSuccess(Method method, GeneratedCredentials response) {
+          @Override
+          public void onSuccess(Method method, GeneratedCredentials response) {
 
-                        genUserUI.onCredentialsReceived(response, new Callback() {
-                            @Override
-                            public void call() {
-                                AuthenticationService.INSTANCE.signinWithAlias(new Credentials(EmbeddedData.surveyId, response.userName, response.password), new MethodCallback<SigninResult>() {
-                                    @Override
-                                    public void onFailure(Method signinMethod, Throwable exception) {
-                                        ErrorReportingService.reportError(new RuntimeException("Failed to generate user", exception));
-                                        genUserUI.onServiceError();
-                                    }
+            genUserUI.onCredentialsReceived(response, new Callback() {
+              @Override
+              public void call() {
+                AuthenticationService.INSTANCE.signinWithAlias(new Credentials(EmbeddedData.surveyId, response.userName, response.password), new MethodCallback<SigninResult>() {
+                  @Override
+                  public void onFailure(Method signinMethod, Throwable exception) {
+                    UncaughtExceptionHandler.reportError(new RuntimeException("Failed to generate user", exception));
+                    genUserUI.onServiceError();
+                  }
 
-                                    @Override
-                                    public void onSuccess(Method signinMethod, SigninResult response) {
-                                        AuthCache.updateRefreshToken(response.refreshToken);
-                                        //TODO: User HTML5 history API to prevent reload
-                                        Window.Location.replace(Window.Location.createUrlBuilder().removeParameter("genUser").buildString());
-                                    }
-                                });
-                            }
-                        });
-                    }
+                  @Override
+                  public void onSuccess(Method signinMethod, SigninResult response) {
+                    AuthCache.updateRefreshToken(response.refreshToken);
+                    //TODO: User HTML5 history API to prevent reload
+                    Window.Location.replace(Window.Location.createUrlBuilder().removeParameter("genUser").buildString());
+                  }
                 });
-            } else if (Window.Location.getParameter(UrlParameterConstants.authTokenKey) != null) {
-                String authToken = Window.Location.getParameter(UrlParameterConstants.authTokenKey);
+              }
+            });
+          }
+        });
+      } else if (Window.Location.getParameter(UrlParameterConstants.authTokenKey) != null) {
+        String authToken = Window.Location.getParameter(UrlParameterConstants.authTokenKey);
 
-                AuthenticationService.INSTANCE.signinWithToken(authToken, new MethodCallback<SigninResult>() {
-                    @Override
-                    public void onFailure(Method signinMethod, Throwable exception) {
-                        if (signinMethod.getResponse().getStatusCode() == Response.SC_UNAUTHORIZED) {
+        AuthenticationService.INSTANCE.signinWithToken(authToken, new MethodCallback<SigninResult>() {
+          @Override
+          public void onFailure(Method signinMethod, Throwable exception) {
+            if (signinMethod.getResponse().getStatusCode() == Response.SC_UNAUTHORIZED) {
 
-                            if (!EmbeddedData.originatingUrl.isEmpty())
-                                Window.Location.replace(EmbeddedData.originatingUrl.getOrDie());
+              if (!EmbeddedData.originatingUrl.isEmpty())
+                Window.Location.replace(EmbeddedData.originatingUrl.getOrDie());
 
-                            // authTokenUI.onSigninAttemptFailed();
-                        } else {
-                            ErrorReportingService.reportError(new RuntimeException("Authentication service error while trying to sign in with URL auth token", exception));
-                            authTokenUI.onAuthenticationServiceError();
-                        }
-                    }
+              // authTokenUI.onSigninAttemptFailed();
+            } else {
+              UncaughtExceptionHandler.reportError(new RuntimeException("Authentication service error while trying to sign in with URL auth token", exception));
+              authTokenUI.onAuthenticationServiceError();
+            }
+          }
 
-                    @Override
-                    public void onSuccess(Method signinMethod, SigninResult response) {
-                        AuthCache.updateRefreshToken(response.refreshToken);
+          @Override
+          public void onSuccess(Method signinMethod, SigninResult response) {
+            AuthCache.updateRefreshToken(response.refreshToken);
 
-                        String urlWithoutToken = Window.Location.createUrlBuilder().removeParameter(UrlParameterConstants.authTokenKey).buildString();
+            String urlWithoutToken = Window.Location.createUrlBuilder().removeParameter(UrlParameterConstants.authTokenKey).buildString();
 
-                        Window.Location.replace(urlWithoutToken);
+            Window.Location.replace(urlWithoutToken);
 
                         /*
                         refreshMethod.header(AuthCache.AUTH_TOKEN_HEADER, response.refreshToken);
@@ -111,53 +109,53 @@ public class RefreshCallback implements RequestCallback {
                         } catch (RequestException e) {
                             refreshCallback.onError(signinMethod.getRequest(), e);
                         }*/
-                    }
-                });
-            } else if (!EmbeddedData.originatingUrl.isEmpty()) {
-                Window.Location.replace(EmbeddedData.originatingUrl.getOrDie());
-            } else {
-                loginUI.show(new Callback1<Credentials>() {
-                    @Override
-                    public void call(Credentials credentials) {
-                        AuthenticationService.INSTANCE.signinWithAlias(credentials, new MethodCallback<SigninResult>() {
-                            @Override
-                            public void onFailure(Method signinMethod, Throwable exception) {
-                                if (signinMethod.getResponse().getStatusCode() == Response.SC_UNAUTHORIZED) {
-                                    loginUI.onSigninAttemptFailed();
-                                } else {
-                                    ErrorReportingService.reportError(new RuntimeException("Authentication service error while trying to sign in with credentials", exception));
-                                    loginUI.onAuthenticationServiceError();
-                                }
-                            }
+          }
+        });
+      } else if (!EmbeddedData.originatingUrl.isEmpty()) {
+        Window.Location.replace(EmbeddedData.originatingUrl.getOrDie());
+      } else {
+        loginUI.show(new Callback1<Credentials>() {
+          @Override
+          public void call(Credentials credentials) {
+            AuthenticationService.INSTANCE.signinWithAlias(credentials, new MethodCallback<SigninResult>() {
+              @Override
+              public void onFailure(Method signinMethod, Throwable exception) {
+                if (signinMethod.getResponse().getStatusCode() == Response.SC_UNAUTHORIZED) {
+                  loginUI.onSigninAttemptFailed();
+                } else {
+                  UncaughtExceptionHandler.reportError(new RuntimeException("Authentication service error while trying to sign in with credentials", exception));
+                  loginUI.onAuthenticationServiceError();
+                }
+              }
 
-                            @Override
-                            public void onSuccess(Method signinMethod, SigninResult response) {
-                                loginUI.hide();
+              @Override
+              public void onSuccess(Method signinMethod, SigninResult response) {
+                loginUI.hide();
 
-                                logger.fine("Login with provided credentials successful");
+                logger.fine("Login with provided credentials successful");
 
-                                AuthCache.updateRefreshToken(response.refreshToken);
+                AuthCache.updateRefreshToken(response.refreshToken);
 
-                                refreshMethod.header(AuthCache.AUTH_TOKEN_HEADER, response.refreshToken);
+                refreshMethod.header(AuthCache.AUTH_TOKEN_HEADER, response.refreshToken);
 
-                                try {
-                                    refreshMethod.send(RefreshCallback.this);
-                                } catch (RequestException e) {
-                                    refreshCallback.onError(signinMethod.getRequest(), e);
-                                }
+                try {
+                  refreshMethod.send(RefreshCallback.this);
+                } catch (RequestException e) {
+                  refreshCallback.onError(signinMethod.getRequest(), e);
+                }
 
-                            }
-                        });
-                    }
-                });
-            }
-        } else {
-            refreshCallback.onResponseReceived(request, response);
-        }
+              }
+            });
+          }
+        });
+      }
+    } else {
+      refreshCallback.onResponseReceived(request, response);
     }
+  }
 
-    @Override
-    public void onError(Request request, Throwable throwable) {
-        refreshCallback.onError(request, throwable);
-    }
+  @Override
+  public void onError(Request request, Throwable throwable) {
+    refreshCallback.onError(request, throwable);
+  }
 }
