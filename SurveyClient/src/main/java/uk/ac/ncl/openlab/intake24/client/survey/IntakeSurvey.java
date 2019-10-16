8a2d79e6e8922ee32f5ce03ee5fba3c09491f2df
Ivan Poliakov <ivpolyakov@gmail.com>
2019-10-16 19:04:39 +0100

    Force first meal to be selected on free entry completion

--- SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/survey/IntakeSurvey.java
+++ SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/survey/IntakeSurvey.java
@@ -44,238 +44,250 @@ import uk.ac.ncl.openlab.intake24.client.survey.prompts.EditMealPrompt;
 import uk.ac.ncl.openlab.intake24.client.survey.prompts.EditTimePrompt;
 
 public class IntakeSurvey implements SurveyStage<Survey> {
-	public final static String FLAG_INTAKE_SURVEY_COMPLETE = "intake-survey-complete";
+    public final static String FLAG_INTAKE_SURVEY_COMPLETE = "intake-survey-complete";
 
-	private final PromptManager promptManager;
-	private final SelectionManager selectionManager;
-	private final StateManager baseStateManager;
+    private final PromptManager promptManager;
+    private final SelectionManager selectionManager;
+    private final StateManager baseStateManager;
 
-	private IntakeStateManager stateManager;
+    private IntakeStateManager stateManager;
 
-	private final Logger log = Logger.getLogger("IntakeSurvey");
+    private final Logger log = Logger.getLogger("IntakeSurvey");
 
-	public IntakeSurvey(StateManager stateManager, PromptManager promptManager, SelectionManager selectionManager,
-			PortionSizeScriptManager scriptManager) {
-		this.baseStateManager = stateManager;
-		this.promptManager = promptManager;
-		this.selectionManager = selectionManager;
-	}
+    public IntakeSurvey(StateManager stateManager, PromptManager promptManager, SelectionManager selectionManager,
+                        PortionSizeScriptManager scriptManager) {
+        this.baseStateManager = stateManager;
+        this.promptManager = promptManager;
+        this.selectionManager = selectionManager;
+    }
 
-	private NavigationPanel navigationPanel;
-	private PromptInterfaceManager interfaceManager;
-	private Callback1<SurveyOperation> applyOperation;
-	private Callback1<Survey> onComplete;
-	private Callback1<Function1<Survey, Survey>> updateIntermediateState;
+    private NavigationPanel navigationPanel;
+    private PromptInterfaceManager interfaceManager;
+    private Callback1<SurveyOperation> applyOperation;
+    private Callback1<Survey> onComplete;
+    private Callback1<Function1<Survey, Survey>> updateIntermediateState;
 
-	private SimpleSurveyStageInterface cachedInterface = null;
-	
-	public void showPrompt(Prompt<Survey, SurveyOperation> prompt) {
-		interfaceManager.applyInterface(prompt, applyOperation, updateIntermediateState);
-	}
+    private SimpleSurveyStageInterface cachedInterface = null;
 
-	private Selection convertToAuto(Selection selection) {
-		return selection.accept(new Selection.Visitor<Selection>() {
-			@Override
-			public Selection visitMeal(Selection.SelectedMeal meal) {
-				return new Selection.SelectedMeal(meal.mealIndex, SelectionMode.AUTO_SELECTION);
-			}
+    public void showPrompt(Prompt<Survey, SurveyOperation> prompt) {
+        interfaceManager.applyInterface(prompt, applyOperation, updateIntermediateState);
+    }
 
-			@Override
-			public Selection visitFood(Selection.SelectedFood food) {
-				return new Selection.SelectedFood(food.mealIndex, food.foodIndex, SelectionMode.AUTO_SELECTION);
-			}
+    private Selection convertToAuto(Selection selection) {
+        return selection.accept(new Selection.Visitor<Selection>() {
+            @Override
+            public Selection visitMeal(Selection.SelectedMeal meal) {
+                return new Selection.SelectedMeal(meal.mealIndex, SelectionMode.AUTO_SELECTION);
+            }
 
-			@Override
-			public Selection visitNothing(Selection.EmptySelection selection) {
-				return new Selection.EmptySelection(SelectionMode.AUTO_SELECTION);
-			}
-		});
-	}
+            @Override
+            public Selection visitFood(Selection.SelectedFood food) {
+                return new Selection.SelectedFood(food.mealIndex, food.foodIndex, SelectionMode.AUTO_SELECTION);
+            }
 
-	public void showNextPrompt() {
-		Survey currentState = stateManager.getCurrentState();
-		
-		Option<Prompt<Survey, SurveyOperation>> nextPrompt = promptManager.nextPromptForSelection(currentState);
+            @Override
+            public Selection visitNothing(Selection.EmptySelection selection) {
+                return new Selection.EmptySelection(SelectionMode.AUTO_SELECTION);
+            }
+        });
+    }
 
-		final Survey stateWithForcedAutoSelection = currentState.withSelection(convertToAuto(currentState.selectedElement));
-		stateManager.updateState(stateWithForcedAutoSelection, false);
+    public void showNextPrompt() {
+        Survey currentState = stateManager.getCurrentState();
 
-		nextPrompt.accept(new SideEffectVisitor<Prompt<Survey, SurveyOperation>>() {
-			@Override
-			public void visitSome(Prompt<Survey, SurveyOperation> prompt) {
-				showPrompt(prompt);
-			}
+        Option<Prompt<Survey, SurveyOperation>> nextPrompt = promptManager.nextPromptForSelection(currentState);
 
-			@Override
-			public void visitNone() {
-				// No prompt available for the current selection
-				// Try to auto-select something else
+        final Survey stateWithForcedAutoSelection = currentState.withSelection(convertToAuto(currentState.selectedElement));
+        stateManager.updateState(stateWithForcedAutoSelection, false);
 
-				Option<Selection> nextSelection = selectionManager.nextSelection(stateWithForcedAutoSelection);
+        nextPrompt.accept(new SideEffectVisitor<Prompt<Survey, SurveyOperation>>() {
+            @Override
+            public void visitSome(Prompt<Survey, SurveyOperation> prompt) {
+                showPrompt(prompt);
+            }
 
-				nextSelection.accept(new SideEffectVisitor<Selection>() {
-					@Override
-					public void visitSome(Selection item) {
-						// Selected something, pull the next prompt for
-						// selection
-						Survey updatedState = stateWithForcedAutoSelection.withSelection(item);
-						// System.out.println ("Auto-select: " + item);
-						stateManager.updateState(updatedState, false);
-						showNextPrompt();
-					}
+            @Override
+            public void visitNone() {
+                // No prompt available for the current selection
+                // Try to auto-select something else
 
-					@Override
-					public void visitNone() {
-						// nothing else to be selected, get next global
-						// prompt
+                Option<Selection> nextSelection = selectionManager.nextSelection(stateWithForcedAutoSelection);
 
-						promptManager.nextGlobalPrompt(stateWithForcedAutoSelection).accept(new SideEffectVisitor<Prompt<Survey, SurveyOperation>>() {
-							@Override
-							public void visitSome(Prompt<Survey, SurveyOperation> prompt) {
-								showPrompt(prompt);
-							}
+                nextSelection.accept(new SideEffectVisitor<Selection>() {
+                    @Override
+                    public void visitSome(Selection item) {
+                        // Selected something, pull the next prompt for
+                        // selection
+                        Survey updatedState = stateWithForcedAutoSelection.withSelection(item);
+                        // System.out.println ("Auto-select: " + item);
+                        stateManager.updateState(updatedState, false);
+                        showNextPrompt();
+                    }
 
-							@Override
-							public void visitNone() {
-								// no associatedFoods left, report completion
-								onComplete.call(stateWithForcedAutoSelection.withFlag(Survey.FLAG_NO_MORE_PROMPTS).withFlag(Survey.FLAG_SKIP_HISTORY));
-							}
-						});
+                    @Override
+                    public void visitNone() {
+                        // nothing else to be selected, get next global
+                        // prompt
 
-					}
-				});
-			}
-		});
-	}
+                        promptManager.nextGlobalPrompt(stateWithForcedAutoSelection).accept(new SideEffectVisitor<Prompt<Survey, SurveyOperation>>() {
+                            @Override
+                            public void visitSome(Prompt<Survey, SurveyOperation> prompt) {
+                                showPrompt(prompt);
+                            }
 
-	@Override
-	public SimpleSurveyStageInterface getInterface(Callback1<Survey> onComplete, Callback2<Survey, Boolean> onIntermediateStateChange) {
-		if (cachedInterface != null) {
-			showNextPrompt();
-			return cachedInterface;
-		} else {
+                            @Override
+                            public void visitNone() {
+                                // no associatedFoods left, report completion
+                                onComplete.call(stateWithForcedAutoSelection.withFlag(Survey.FLAG_NO_MORE_PROMPTS).withFlag(Survey.FLAG_SKIP_HISTORY));
+                            }
+                        });
 
-			navigationPanel = new NavigationPanel(baseStateManager.getCurrentState());
+                    }
+                });
+            }
+        });
+    }
 
-			stateManager = new IntakeStateManager(baseStateManager, navigationPanel);
+    @Override
+    public SimpleSurveyStageInterface getInterface(Callback1<Survey> onComplete, Callback2<Survey, Boolean> onIntermediateStateChange) {
+        if (cachedInterface != null) {
+            showNextPrompt();
+            return cachedInterface;
+        } else {
 
-			this.onComplete = onComplete;
+            navigationPanel = new NavigationPanel(baseStateManager.getCurrentState());
 
-			final FlowPanel promptPanel = new FlowPanel();
-			promptPanel.getElement().setId("intake24-prompt-panel");
-			
-			interfaceManager = new PromptInterfaceManager(promptPanel);
+            stateManager = new IntakeStateManager(baseStateManager, navigationPanel);
 
-			applyOperation = new Callback1<SurveyOperation>() {
-				@Override
-				public void call(final SurveyOperation op) {
-					op.accept(new SurveyOperation.SideEffectVisitor() {
-						@Override
-						public void visitNoChange() {
-							showNextPrompt();
-						}
+            this.onComplete = onComplete;
 
-						@Override
-						public void visitEditFoodsRequest(int mealIndex, boolean addDrink) {
-							showPrompt(new PromptAdapter.ForMeal(mealIndex, new EditMealPrompt(stateManager.getCurrentState().meals.get(mealIndex), addDrink)));
-						}
+            final FlowPanel promptPanel = new FlowPanel();
+            promptPanel.getElement().setId("intake24-prompt-panel");
 
-						@Override
-						public void visitEditTimeRequest(int mealIndex) {
-							Survey currentState = stateManager.getCurrentState();
-							showPrompt(new PromptAdapter.ForMeal(mealIndex, new EditTimePrompt(currentState.meals.get(mealIndex).name, currentState.meals
-									.get(mealIndex).time.getOrDie())));
-						}
+            interfaceManager = new PromptInterfaceManager(promptPanel);
 
-						@Override
-						public void visitUpdate(SurveyOperation.Update update) {
-							Survey newState = update.update.apply(stateManager.getCurrentState());
-							stateManager.updateState(newState, update.makeHistoryEntry);
-							showNextPrompt();
-						}
+            applyOperation = new Callback1<SurveyOperation>() {
+                @Override
+                public void call(final SurveyOperation op) {
+                    op.accept(new SurveyOperation.SideEffectVisitor() {
+                        @Override
+                        public void visitNoChange() {
+                            showNextPrompt();
+                        }
 
-						@Override
-						public void visitDeleteMealRequest(int mealIndex, boolean showConfirmation) {
-							if (showConfirmation) {								
-								showPrompt(new DeleteMealPrompt(mealIndex, stateManager.getCurrentState().meals.get(mealIndex)));
-							} else {
-								Survey state = stateManager.getCurrentState();
-								stateManager.updateState(state.minusMeal(mealIndex).withSelection(new Selection.EmptySelection(SelectionMode.AUTO_SELECTION)), true);
-								showNextPrompt();
-							}
-						}
+                        @Override
+                        public void visitEditFoodsRequest(int mealIndex, boolean addDrink) {
+                            showPrompt(new PromptAdapter.ForMeal(mealIndex, new EditMealPrompt(stateManager.getCurrentState().meals.get(mealIndex), addDrink)));
+                        }
 
-						@Override
-						public void visitSplitFood(int mealIndex, int foodIndex, PVector<FoodEntry> splitInto) {
-							Survey curState = stateManager.getCurrentState();
+                        @Override
+                        public void visitEditTimeRequest(int mealIndex) {
+                            Survey currentState = stateManager.getCurrentState();
+                            showPrompt(new PromptAdapter.ForMeal(mealIndex, new EditTimePrompt(currentState.meals.get(mealIndex).name, currentState.meals
+                                    .get(mealIndex).time.getOrDie())));
+                        }
 
-							PVector<FoodEntry> updatedFoods = curState.meals.get(mealIndex).foods.minus(foodIndex).plusAll(foodIndex, splitInto);
+                        @Override
+                        public void visitUpdate(SurveyOperation.Update update) {
+                            boolean wasFreeEntryComplete = stateManager.getCurrentState().freeEntryComplete();
+                            Survey newState = update.update.apply(stateManager.getCurrentState());
 
-							Survey newState = curState.withMeals(curState.meals.with(mealIndex, curState.meals.get(mealIndex).withFoods(updatedFoods)));
-							stateManager.updateState(newState, true);
-							showNextPrompt();
-						}
+                            // When free entry is complete, force selection to go back to the first meal so that
+							// question order make sense.
 
-						@Override
-						public void visitAddMeal(int selectedIndex) {
-							showPrompt(new AddMealPrompt(selectedIndex));
-						}
-					});
-				}
-			};
+							// Otherwise, the last entered food will remain selected, then foods in the last entered meal and
+							// then the first meal.
 
-			updateIntermediateState = new Callback1<Function1<Survey, Survey>>() {
-				@Override
-				public void call(Function1<Survey, Survey> update) {
-					stateManager.updateState(update.apply(stateManager.getCurrentState()), true);
-				}
-			};
+                            if (!wasFreeEntryComplete && newState.freeEntryComplete()) {
+                                if (newState.meals.size() > 0)
+                                    newState = newState.withSelection(new Selection.SelectedMeal(0, SelectionMode.AUTO_SELECTION));
+                            }
+                            stateManager.updateState(newState, update.makeHistoryEntry);
+                            showNextPrompt();
+                        }
 
-			Callback1<Selection> requestSelection = new Callback1<Selection>() {
-				@Override
-				public void call(Selection sel) {
-					log.info("Selection request: " + sel.toString());
+                        @Override
+                        public void visitDeleteMealRequest(int mealIndex, boolean showConfirmation) {
+                            if (showConfirmation) {
+                                showPrompt(new DeleteMealPrompt(mealIndex, stateManager.getCurrentState().meals.get(mealIndex)));
+                            } else {
+                                Survey state = stateManager.getCurrentState();
+                                stateManager.updateState(state.minusMeal(mealIndex).withSelection(new Selection.EmptySelection(SelectionMode.AUTO_SELECTION)), true);
+                                showNextPrompt();
+                            }
+                        }
 
-					Survey currentState = stateManager.getCurrentState();
-					//if (!currentState.selectedElement.equals(sel)) {
-						log.info("Selection changed");
-						Survey withSelection = currentState.withSelection(sel);
-						stateManager.updateState(withSelection, true);
-						showNextPrompt();
-					//} else {
-						// FIXME: should actually determine if selection change would show the same prompt
-						//log.info("Element already selected, request ignored");
-					//}
-				}
-			};
+                        @Override
+                        public void visitSplitFood(int mealIndex, int foodIndex, PVector<FoodEntry> splitInto) {
+                            Survey curState = stateManager.getCurrentState();
 
-			final Callback requestAddMeal = new Callback() {
-				@Override
-				public void call() {
-					showPrompt(new AddMealPrompt(0));
-				}
-			};
+                            PVector<FoodEntry> updatedFoods = curState.meals.get(mealIndex).foods.minus(foodIndex).plusAll(foodIndex, splitInto);
 
-			navigationPanel.setCallbacks(requestSelection, requestAddMeal);
-			
-			FlowPanel content = new FlowPanel();
-			
-			content.getElement().setId("intake24-survey-container");
+                            Survey newState = curState.withMeals(curState.meals.with(mealIndex, curState.meals.get(mealIndex).withFoods(updatedFoods)));
+                            stateManager.updateState(newState, true);
+                            showNextPrompt();
+                        }
 
-			content.add(navigationPanel);
-			content.add(promptPanel);
-			
-			FlowPanel clearDiv = new FlowPanel();
-			clearDiv.addStyleName("intake24-clear-floats");
-			
-			content.add(clearDiv);
-			
+                        @Override
+                        public void visitAddMeal(int selectedIndex) {
+                            showPrompt(new AddMealPrompt(selectedIndex));
+                        }
+                    });
+                }
+            };
 
-			showNextPrompt();
+            updateIntermediateState = new Callback1<Function1<Survey, Survey>>() {
+                @Override
+                public void call(Function1<Survey, Survey> update) {
+                    stateManager.updateState(update.apply(stateManager.getCurrentState()), true);
+                }
+            };
 
-			cachedInterface = new SimpleSurveyStageInterface(content, IntakeSurvey.class.getSimpleName());
+            Callback1<Selection> requestSelection = new Callback1<Selection>() {
+                @Override
+                public void call(Selection sel) {
+                    log.info("Selection request: " + sel.toString());
 
-			return cachedInterface;
-		}
-	}
+                    Survey currentState = stateManager.getCurrentState();
+                    //if (!currentState.selectedElement.equals(sel)) {
+                    log.info("Selection changed");
+                    Survey withSelection = currentState.withSelection(sel);
+                    stateManager.updateState(withSelection, true);
+                    showNextPrompt();
+                    //} else {
+                    // FIXME: should actually determine if selection change would show the same prompt
+                    //log.info("Element already selected, request ignored");
+                    //}
+                }
+            };
+
+            final Callback requestAddMeal = new Callback() {
+                @Override
+                public void call() {
+                    showPrompt(new AddMealPrompt(0));
+                }
+            };
+
+            navigationPanel.setCallbacks(requestSelection, requestAddMeal);
+
+            FlowPanel content = new FlowPanel();
+
+            content.getElement().setId("intake24-survey-container");
+
+            content.add(navigationPanel);
+            content.add(promptPanel);
+
+            FlowPanel clearDiv = new FlowPanel();
+            clearDiv.addStyleName("intake24-clear-floats");
+
+            content.add(clearDiv);
+
+
+            showNextPrompt();
+
+            cachedInterface = new SimpleSurveyStageInterface(content, IntakeSurvey.class.getSimpleName());
+
+            return cachedInterface;
+        }
+    }
 }
