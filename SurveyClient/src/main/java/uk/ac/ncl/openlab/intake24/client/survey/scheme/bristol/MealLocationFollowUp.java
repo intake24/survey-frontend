85a8f2c9ee5137681fc388b719a2992abf616bc6
Lukas Hroch <lukashroch@gmail.com>
2023-06-13 17:44:25 +0100

    fix(scheme): checkbox prompt

--- SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/survey/scheme/bristol/MealLocationFollowUp.java
+++ SurveyClient/src/main/java/uk/ac/ncl/openlab/intake24/client/survey/scheme/bristol/MealLocationFollowUp.java
@@ -10,10 +10,11 @@ import uk.ac.ncl.openlab.intake24.client.survey.*;
 import uk.ac.ncl.openlab.intake24.client.survey.prompts.MealOperation;
 import uk.ac.ncl.openlab.intake24.client.survey.prompts.MultipleChoiceQuestionOption;
 import uk.ac.ncl.openlab.intake24.client.survey.prompts.messages.PromptMessages;
-import uk.ac.ncl.openlab.intake24.client.survey.prompts.simple.RadioButtonPrompt;
+import uk.ac.ncl.openlab.intake24.client.survey.prompts.simple.CheckListPrompt;
 
 public class MealLocationFollowUp implements PromptRule<Meal, MealOperation> {
 
+    private static final String MEAL_LOCATION_FOLLOW_UP_COMPLETE = "mealLocationFollowUpComplete";
     private static final String MEAL_LOCATION_FOLLOW_UP_KEY = "mealLocationFollowUp";
 
     private static final String promptTemplate = "<p>Where were you when you had your $meal?</p>";
@@ -29,18 +30,25 @@ public class MealLocationFollowUp implements PromptRule<Meal, MealOperation> {
 
     @Override
     public Option<Prompt<Meal, MealOperation>> apply(Meal state, SelectionMode selectionType, PSet<String> surveyFlags) {
-        if (!state.customData.containsKey(MEAL_LOCATION_FOLLOW_UP_KEY) &&
+        if (!state.flags.contains(MEAL_LOCATION_FOLLOW_UP_COMPLETE) &&
                 state.customData.containsKey(MealLocation.MEAL_LOCATION_KEY) &&
                 !state.customData.get(MealLocation.MEAL_LOCATION_KEY).equals("Yes") &&
                 !state.isEmpty() && state.portionSizeComplete()) {
             SafeHtml promptSafeText = SafeHtmlUtils.fromSafeConstant(promptTemplate.replace("$meal", SafeHtmlUtils.htmlEscape(state.name.toLowerCase())));
 
-            RadioButtonPrompt prompt = new RadioButtonPrompt(promptSafeText, MealLocationFollowUp.class.getSimpleName(),
-                    options, PromptMessages.INSTANCE.mealComplete_continueButtonLabel(),
-                    "mealLocationFollowUp");
+            CheckListPrompt prompt = new CheckListPrompt(promptSafeText, MealLocationFollowUp.class.getSimpleName(),
+                    options, PromptMessages.INSTANCE.mealComplete_continueButtonLabel());
 
-            return Option.some(PromptUtil.asMealPrompt(prompt, answer ->
-                    MealOperation.setCustomDataField(MEAL_LOCATION_FOLLOW_UP_KEY, answer.getValue())));
+            return Option.some(PromptUtil.asMealPrompt(prompt, answers -> {
+                if (!answers.isEmpty()) {
+                    String mealLocationFollowUpValue = answers.stream()
+                            .map(answer -> answer.getValue())
+                            .reduce("", (s1, s2) -> s1 + (s1.isEmpty() ? "" : ", ") + s2);
+
+                    return MealOperation.update(meal -> meal.withCustomDataField(MEAL_LOCATION_FOLLOW_UP_KEY, mealLocationFollowUpValue).withFlag(MEAL_LOCATION_FOLLOW_UP_COMPLETE));
+                } else
+                    return MealOperation.update(meal -> meal.withFlag(MEAL_LOCATION_FOLLOW_UP_COMPLETE));
+            }));
         } else {
             return Option.none();
         }
